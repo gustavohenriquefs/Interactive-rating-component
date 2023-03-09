52c016bf8c23f5637c517f2eedf886636e707481
Gustavo Henrique <gustavohenriquefs.dev@gmail.com>
2023-03-09 09:23:24 -0300

    remove modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { CardRatingComponent } from './pages/card-rating/card-rating.component';
+import { CardThankYouComponent } from './pages/card-thank-you/card-thank-you.component';
+
+const routes: Routes = [{ 
+  path: 'rating', 
+  component: CardRatingComponent
+}, { 
+  path: 'thank-you/:rating', 
+  component: CardThankYouComponent
+}, {
+  path: '**',
+  redirectTo: '/rating',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

79cd24589b0dc602ac9ce3568d08ccfa468de8a4
Gustavo Henrique <gustavohenriquefs.dev@gmail.com>
2023-03-08 22:22:47 -0300

    add accebility props in rating button list

--- src/app/shared/components/molecules/rating-button-list/rating-button-list.component.ts
+++ src/app/shared/components/molecules/rating-button-list/rating-button-list.component.ts
@@ -15,6 +15,12 @@ export class RatingButtonListComponent implements OnInit {
 
   @Input() 
   public buttonsAmount: number = 5;
+
+  @Input()
+  public idQuestion: string = "";
+
+  @Input()
+  public idDescription: string = "";
   
   public buttonList: RatingButton[] = [];
 
@@ -26,13 +32,8 @@ export class RatingButtonListComponent implements OnInit {
 
   private initilizeButtons(): void {
     for(let buttonRating = 1; buttonRating <= this.buttonsAmount; buttonRating ++) {
-      let ariaLabel = `Avaliado como ${buttonRating} `;
-      
-      if(buttonRating === 1) {
-        ariaLabel += 'estrela.';
-      } else {
-        ariaLabel += 'estralas.';
-      }
+      let ariaLabel = `Avaliado em ${buttonRating} de ${this.buttonsAmount}`;
+
 
       const newButton: RatingButton = {
         rating: buttonRating,
